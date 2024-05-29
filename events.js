20bd0f288c265efa2caf9ad0261cb776b80fcb96
Denis Nadarevic <dnadarevic@descartes.com>
2024-05-29 15:25:35 -0400

    denis massive commit

---
+++ events.js
@@ -0,0 +1,44 @@
+// Hi, Emily! This is where you can add events to the website. 
+// I highly recommend using the following format for each event:
+// name, location, link, startDate, endDate
+// Try to keep the names short and concise to avoid issues with styling.
+// If you have any questions, feel free to ask me. - Denis
+
+
+window.events = [
+  {
+    name: 'Event Name 1',
+    location: 'London, ON',
+    link: 'https://example.com',
+    startDate: '2022-07-24',
+    endDate: '2024-07-24',
+  },
+  {
+    name: 'Event Name 2',
+    location: 'Hamilton, ON',
+    link: 'https://example.com',
+    startDate: '2024-10-23',
+    endDate: '2024-11-24',
+  },
+  {
+    name: 'Event Name 3',
+    location: 'Toronto, ON',
+    link: 'https://example.com',
+    startDate: '2024-12-24',
+    endDate: '2024-12-24',
+  },
+  {
+    name: 'Event Name 4',
+    location: 'Ottawa, ON',
+    link: 'https://example.com',
+    startDate: '2025-01-24',
+    endDate: '2025-01-24',
+  },
+  {
+    name: 'Event Name 5',
+    location: 'Windsor, ON',
+    link: 'https://example.com',
+    startDate: '2025-02-24',
+    endDate: '2025-02-24',
+  }
+];
