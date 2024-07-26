98eb8cef0cfe553ff2c09ba276d5e64d95210baa
Emily Boice <46850194+EmilyB130@users.noreply.github.com>
2024-07-26 09:52:47 -0400

    Update events.js

--- events.js
+++ events.js
@@ -7,27 +7,6 @@
 
 window.events = [
   {
-    name: 'Art In The Park',
-    location: 'Walkerville',
-    link: 'https://artintheparkwindsor.com/',
-    startDate: '2024-06-01',
-    endDate: '2024-06-02',
-  },
-  {
-    name: 'Cindys Home And Garden',
-    location: 'Kingsville',
-    link: 'https://cindysgarden.com/events/',
-    startDate: '2024-06-15',
-    endDate: '2024-06-15',
-  },
-  {
-    name: 'Art In The Barn',
-    location: 'Dorchester',
-    link: 'https://www.artinthebarndorchester.com/',
-    startDate: '2024-07-13',
-    endDate: '2024-07-14',
-  },
-  {
     name: 'Cindys Home And Garden',
     location: 'Kingsville',
     link: 'https://cindysgarden.com/events/',
@@ -38,7 +17,64 @@ window.events = [
     name: 'Dresden Night Market',
     location: 'Dresden',
     link: 'https://night-market.ca/',
-    startDate: '2025-07-27',
-    endDate: '2025-07-27',
+    startDate: '2024-07-27',
+    endDate: '2024-07-27',
+  },
+   {
+    name: 'Art By The River',
+    location: 'Amherstburg',
+    link: 'http://www.gibsonartgallery.com/Art-by-the-River.html',
+    startDate: '2024-08-24',
+    endDate: '2024-08-25',
+  },
+  {
+    name: 'Walkerville Brewery',
+    location: 'Walkerville',
+    link: 'https://www.walkervillebrewery.com/wddnm',
+    startDate: '2024-08-30',
+    endDate: '2024-08-30',
+  },
+  {
+    name: 'St.George Apple Fest',
+    location: 'St.George',
+    link: 'https://stgeorgeapplefest.ca/',
+    startDate: '2024-09-21',
+    endDate: '2024-09-22',
+  },
+  {
+    name: 'Lambton Fall Colour & Creaft Festival',
+    location: 'Grand Bend',
+    link: 'https://www.lambtonmuseums.ca/en/lambton-heritage-museum/lambton-fall-colour-craft-festival.aspx',
+    startDate: '2024-10-19',
+    endDate: '2024-10-20',
+  },
+  {
+    name: 'Cindys Home And Garden',
+    location: 'Kingsville',
+    link: 'https://cindysgarden.com/events/',
+    startDate: '2024-11-02',
+    endDate: '2024-11-03',
+  },
+  {
+    name: 'The Mom Market Collective',
+    location: 'Lasalle Event Center',
+    link: 'https://www.instagram.com/themommarket_windsor',
+    startDate: '2024-11-23',
+    endDate: '2024-11-23',
+  },
+   {
+    name: 'London Christmas Craft Show',
+    location: 'London - Western Fairgorund | Agriplex',
+    link: 'https://londoncraftshows.com/',
+    startDate: '2024-11-28',
+    endDate: '2024-12-01',
+  },
+  {
+    name: 'The Mom Market Collective',
+    location: 'Lasalle Event Center',
+    link: 'https://www.instagram.com/themommarket_windsor',
+    startDate: '2024-12-14',
+    endDate: '2024-12-25',
   }
+  
 ];
